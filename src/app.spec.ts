5f6849bb09ea5151815f33d8bbb58175e41c7b9d
Dan Nguyen <nghuudan@gmail.com>
2018-09-20 18:59:25 -0400

    Add helmet for more secure Express headers

--- src/app.spec.ts
+++ src/app.spec.ts
@@ -1,11 +1,17 @@
+import helmet from 'helmet';
 import app from './app';
 import routes from './routes';
 
+jest.mock('helmet');
 jest.mock('./util/logger');
 
 describe('app', () => {
   it('should call listen once', () => {
     expect(app.listen).toBeCalledTimes(1);
+  });
+
+  it('should call use with helmet()', () => {
+    expect(app.use).toBeCalledWith(helmet());
   });
 
   it('should call use with routes', () => {
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import helmet from 'helmet';
 import routes from './routes';
 import logger from './util/logger';
 
 const app = express();
+app.use(helmet());
 app.use(routes);
 app.listen(3333, () => logger.info('Express server listening on port 3333'));
 
